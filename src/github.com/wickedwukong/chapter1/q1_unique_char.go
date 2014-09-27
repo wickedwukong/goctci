4fb58f31ec31b20ba3c27a0bb037c24b831b4f3b
Xuemin Guan <gxuemin@gmail.com>
2014-09-27 23:44:18 +0100

    implemetingit

--- src/github.com/wickedwukong/chapter1/q1_unique_char.go
+++ src/github.com/wickedwukong/chapter1/q1_unique_char.go
@@ -1,5 +1,16 @@
 package chapter1
 
 func HasUniqueChar(value string) bool {
-	return true
+	records := make([]int, 256)
+
+	for _, c := range value {
+		index := c - 'a'
+		if (records[index] == 0) {
+			records[index] = 1
+		} else {
+			return false
+		}
+	}	
+
+		return true
 }
--- src/github.com/wickedwukong/chapter1/q1_unique_char_test.go
+++ src/github.com/wickedwukong/chapter1/q1_unique_char_test.go
@@ -4,6 +4,24 @@ import "testing"
 
 func TestHasUniqueChars(t *testing.T) {
 	if (!HasUniqueChar("")) {
-        t.Error("Expected 1.5, got ")
+        t.Error("Expected true, got false")
 	}
+
+   if (!HasUniqueChar("ab")) {
+        t.Error("Expected false, got true")
+	}
+
+	if (!HasUniqueChar("a")) {
+        t.Error("Expected true, got false")
+	}
+
+   if (HasUniqueChar("aa")) {
+        t.Error("Expected false, got true")
+	}
+
+   if (HasUniqueChar("aba")) {
+        t.Error("Expected false, got true")
+	}
+
+
 }
