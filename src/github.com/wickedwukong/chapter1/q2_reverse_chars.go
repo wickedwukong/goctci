a72ed4d5fd605a914aa8e556a36a0e5335761e04
Xuemin Guan <gxuemin@gmail.com>
2014-09-29 21:30:43 +0100

    testing nil string pointer

--- src/github.com/wickedwukong/chapter1/q2_reverse_chars.go
+++ src/github.com/wickedwukong/chapter1/q2_reverse_chars.go
@@ -1,6 +1,10 @@
 package chapter1
 
 func Reverse(valuePointer *string) {
+	if (valuePointer == nil) {
+		return
+	}
+	
 	var reversed []byte
 
 	for i := len(*valuePointer) - 1; i >= 0; i-- {
--- src/github.com/wickedwukong/chapter1/q2_reverse_chars_test.go
+++ src/github.com/wickedwukong/chapter1/q2_reverse_chars_test.go
@@ -5,8 +5,32 @@ import (
  	"github.com/stretchr/testify/assert"
  )
 
-func TestReverse(t *testing.T) {
+func TestReverseEmptyString(t *testing.T) {
+	value := ""
+	Reverse(&value)
+	assert.Equal(t, value, "", "'' should be reversed to be ''")
+}
+
+func TestReverseSingleCharString(t *testing.T) {
+	value := "a"
+	Reverse(&value)
+	assert.Equal(t, value, "a", "'a' should be reversed to be 'a'")
+}
+
+func TestReverseTwoRepeatedCharString(t *testing.T) {
+	value := "aa"
+	Reverse(&value)
+	assert.Equal(t, value, "aa", "'aa' should be reversed to be 'aa'")
+}
+
+func TestReverseLongString(t *testing.T) {
 	value := "abcd"
 	Reverse(&value)
-	assert.Equal(t, value, "dcba", "'abcd' should be reversed to 'dcba'")
+	assert.Equal(t, value, "dcba", "'abcd' should be reversed to be 'dcba'")
+}
+
+func TestReverseNilStringPointer(t *testing.T) {
+	var value *string
+	Reverse(value)
+	assert.Equal(t, value, (*string)(nil), "nil string pointer should be still be a nil string pointer")
 }
